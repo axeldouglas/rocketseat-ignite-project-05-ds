ec2c167b670549686c7ae960e525ec9d9b1b6deb
Axel Santos <axel.douglasss@gmail.com>
2022-10-26 11:43:02 -0300

    fix: add subpath to Storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/';
+      config.base = '/rocketseat-ignite-project-05-ds/';
     }
 
     return config;
